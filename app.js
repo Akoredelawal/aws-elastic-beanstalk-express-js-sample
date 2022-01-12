07c149ea8164a254f3089757ed8062491e622bfe
Akoredelawal <koredelawal02@gmail.com>
2022-01-11 21:41:44 -0500

    famzoooo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('i did this myself!'));
+app.get('/', (req, res) => res.send('i did this myself mahad!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
